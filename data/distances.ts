16fb799f43b2fd150f5090c1edc31c605d45a6d4
devashmit <devvv0264@gmail.com>
2026-06-17 22:31:09 +0545

    Add nearby amenities to location section: grocery stores, medical clinic, schools, restaurants, beaches

--- data/distances.ts
+++ data/distances.ts
@@ -7,4 +7,9 @@ export interface Distance {
 
 export const distances: Distance[] = [
   { place: "Kundapura Town", distance: "", time: "3 mins", icon: "Building2" },
+  { place: "Grocery Stores & Markets", distance: "", time: "3 mins", icon: "ShoppingCart" },
+  { place: "Medical Clinic & Hospital", distance: "", time: "3 mins", icon: "Stethoscope" },
+  { place: "Schools & Education", distance: "", time: "3 mins", icon: "BookOpen" },
+  { place: "Restaurants & Cafes", distance: "", time: "3 mins", icon: "UtensilsCrossed" },
+  { place: "Beach Access", distance: "", time: "3 mins", icon: "Waves" },
 ]
